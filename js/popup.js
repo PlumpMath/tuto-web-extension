09843e777930693eb10b4e9201c7c4d145e03218
Michael Cornillon <contact@dwaps.fr>
2017-02-17 15:22:38 +0100

    Module 1 : Déclaration de la browser action dans le manifest

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+/**
+* Auteur : 	DWAPS Formation - Michael Cornillon
+* Cours :	Tutoriel web extension
+* Mail : 	contact@dwaps.fr
+* Tel :		0651279211
+* Site : 	http://dwaps.fr
+* Date : 	16/02/2017
+**/
