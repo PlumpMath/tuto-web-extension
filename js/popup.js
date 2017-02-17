951ba5e272eea7b675b4f3f82f07f1f92c3c6482
Michael Cornillon <contact@dwaps.fr>
2017-02-17 16:12:45 +0100

    Module 1 : Ecriture du code d'états du bouton de la popup

--- js/popup.js
+++ js/popup.js
@@ -7,3 +7,41 @@
 * Date : 	16/02/2017
 **/
 
+// Booléen d'activation/désactivation du module
+var isActive = false;
+
+document
+	.addEventListener(
+		"DOMContentLoaded",
+		function()
+		{
+			// On récupère le bouton de la popup de l'extension
+			var bt = document.querySelector("#btPopupDWAPS");
+
+			// Listener
+			bt.addEventListener(
+				"click",
+				function()
+				{
+					isActive = !isActive;
+
+					// Selon l'état du booléen
+					// On modifie le bouton
+					if(isActive)
+					{
+						bt.className = "active";
+						bt.innerHTML = "ON";
+					}
+					else
+					{
+						bt.className = "inactive";
+						bt.innerHTML = "OFF";
+					}
+
+					// Fermeture de la popup
+					window.close();
+				}
+			);
+		}
+	)
+;
